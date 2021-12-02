7944898c541730318c1e67f5069d2aaf96ca88da
niks87 <94444447+niks87@users.noreply.github.com>
2021-12-02 11:54:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 gugu
 >>> f = open("test.txt")    # open file in current directory
 >>> f = open("C:/Python38/README.txt")  # specifying full path
+# Import os module to read directory
+import os
+
+# Set the directory path
+path = '/home/fahmida/projects/bin'
+
+# Read the content of the file
+files = os.listdir(path)
+
+# Print the content of the directory
+for file in files:
+    print(file)
