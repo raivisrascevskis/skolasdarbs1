77765d9a8337246e8dce5a5db9066ce88a0c8cb0
niks87 <94444447+niks87@users.noreply.github.com>
2021-12-02 12:58:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,26 @@ path = '/home/fahmida/projects/bin'
 # Read the content of the file
 files = os.listdir(path)
 
-# Print the content of the directory
+  # Print the content of the directory
 for file in files:
     print(file)
+# Define a dictionary
+customers = {'06753':'Mehzabin Afroze','02457':'Md. Ali',
+'02834':'Mosarof Ahmed','05623':'Mila Hasan', '07895':'Yaqub Ali'}
+
+# Append a new data
+customers['05634'] = 'Mehboba Ferdous'
+
+print("The customer names are:")
+# Print the values of the dictionary
+for customer in customers:
+    print(customers[customer])
+
+# Take customer ID as input to search
+name = input("Enter customer ID:")
+
+# Search the ID in the dictionary
+for customer in customers:
+    if customer == name:
+        print(customers[customer])
+        break
